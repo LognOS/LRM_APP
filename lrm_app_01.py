cf9dbcf1b97e1e9e865a89b5b4c3956dafaa0046
Facundo <facundo.bassi@vcubo.co>
2023-07-17 13:57:01 -0300

    Updating lrm app

--- lrm_app_01.py
+++ lrm_app_01.py
@@ -5,10 +5,16 @@ import streamlit as st
 from psycopg2 import sql
 import datetime
 import plotly.express as px
-from utils_uploader import create_histogram, create_conn  # Import the function from utils.py
+import plotly.graph_objects as go
+from utils_uploader import create_histogram, create_conn, heat_mapper  # Import the function from utils.py
 
 
-st.set_page_config(layout="wide")
+st.set_page_config(
+   page_title="LognOS",
+   page_icon="",
+   layout="wide",
+   initial_sidebar_state="expanded",
+)
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
@@ -19,6 +25,8 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 palettes = {
     "Pastel": px.colors.qualitative.Pastel,
+    "Pastel1": px.colors.qualitative.Pastel1,
+    "Pastel2": px.colors.qualitative.Pastel2,
     "D3": px.colors.qualitative.D3,
     "G10": px.colors.qualitative.G10,
     "T10": px.colors.qualitative.T10,
@@ -62,13 +70,33 @@ if not st.session_state.df_dict:
     st.session_state.df_dict["pre_event_control"] = query_table("pre_event_control")
     st.session_state.df_dict["pos_event_control"] = query_table("pos_event_control")
 
-#Display projects
-#project_name = st.selectbox("PROJECT", ["Yanacocha WTP2"])
+
+st.write("**PROJECT RISKS REVIEW**")
+h1, h2, h3 = st.columns(3)
+
+# Create histograms using the imported function
+with h1:
+    st.write("RISKS by OWNER FUNCTION")
+    fig_owner = create_histogram(st.session_state.df_dict["risk_main"], "OWN. FUNC", "RISKS by OWNER FUNCTION", palettes["Pastel"])
+    st.plotly_chart(fig_owner)
+with h2:
+    st.write("RISKS by GRADE")
+    fig_grade = create_histogram(st.session_state.df_dict["risk_main"], "GRADE", "RISKS by GRADE", palettes["Pastel1"])
+    st.plotly_chart(fig_grade)
+
+with h3:
+    st.write("RISKS by FUNCTION/GRADE")
+    #Display projects
+    #project_name = st.selectbox("PROJECT", ["Yanacocha WTP2"])
+    st.plotly_chart(heat_mapper(st.session_state.df_dict["risk_main"], "RISKS by GRADE/FUNCTION"))
+
 
 with st.expander("SELECT"):
+    r1, r2, r3 = st.columns(3)
 # Display dropdown for owner selection
-    owners = ["All"] + st.session_state.df_dict["risk_main"]["OWNER"].unique().tolist()
-    selected_owner = st.selectbox("OWNER", owners, index=owners.index(st.session_state.selected_owner))
+    with r1:
+        owners = ["All"] + st.session_state.df_dict["risk_main"]["OWN. FUNC"].unique().tolist()
+        selected_owner = st.selectbox("OWNER FUNCTION", owners, index=owners.index(st.session_state.selected_owner))
 
 
     if selected_owner != st.session_state.selected_owner:
@@ -78,13 +106,14 @@ with st.expander("SELECT"):
 
     # Filter risks based on selected owner
     if st.session_state.selected_owner != "All":
-        risks = st.session_state.df_dict["risk_main"][st.session_state.df_dict["risk_main"]["OWNER"] == st.session_state.selected_owner]
+        risks = st.session_state.df_dict["risk_main"][st.session_state.df_dict["risk_main"]["OWN. FUNC"] == st.session_state.selected_owner]
     else:
         risks = st.session_state.df_dict["risk_main"]
 
     # Display dropdown for risk selection
-    risks_display = ["All"] + [f'{row["TITLE"]}  [{row["RISK ID"]}]' for _, row in risks.iterrows()]
-    st.session_state.selected_risk = st.selectbox("RISK:", risks_display, index=risks_display.index(st.session_state.selected_risk))
+    with r2:
+        risks_display = ["All"] + [f'{row["TITLE"]}  [{row["RISK ID"]}]' for _, row in risks.iterrows()]
+        st.session_state.selected_risk = st.selectbox("RISK:", risks_display, index=risks_display.index(st.session_state.selected_risk))
 
 # Display relevant dataframes if a risk is selected
 if st.session_state.selected_risk != "All":
@@ -96,15 +125,9 @@ if st.session_state.selected_risk != "All":
     pos_event_df = st.session_state.df_dict["pos_event_control"][st.session_state.df_dict["pos_event_control"]["RISK ID"] == risk_id]
     st.dataframe(pos_event_df[["RISK ID", "CONTROL", "DESCRIPTION", "OWNER"]])
 
-# Create histograms using the imported function
-fig_owner = create_histogram(st.session_state.df_dict["risk_main"], "OWNER", "Number of Risks by Owner", color_discrete_sequence=palettes["Pastel"])
-st.plotly_chart(fig_owner)
+    with st.expander("DB REVIEW"):
+        table_names = ["None"]+get_table_names()
+        selected_table = st.selectbox("Select table", table_names)
+        if selected_table != "None":
+            st.dataframe(st.session_state.df_dict[selected_table])
 
-fig_grade = create_histogram(st.session_state.df_dict["risk_main"], "GRADE", "Number of Risks by Grade", color_discrete_sequence=palettes["D3"])
-st.plotly_chart(fig_grade)
-
-with st.expander("DB REVIEW"):
-    table_names = ["None"]+get_table_names()
-    selected_table = st.selectbox("Select table", table_names)
-    if selected_table != "None":
-        st.dataframe(st.session_state.df_dict[selected_table])
--- utils_uploader.py
+++ utils_uploader.py
@@ -1,4 +1,5 @@
 import plotly.express as px
+import plotly.graph_objects as go
 import psycopg2
 import streamlit as st
 
@@ -16,10 +17,73 @@ def test_conn():
     except psycopg2.Error as e:
         st.write(f"Unable to connect to the database. The error {e} occurred.")
 
-def create_histogram(df, column, title, color_discrete_sequence=None):
-    """Create a histogram using Plotly."""
-    counts = df[column].value_counts()
-    fig = px.histogram(counts, x=counts.index, y=counts.values, nbins=len(counts),
-                       labels={'x':column, 'y':'Number of Risks'}, color=counts.index,
-                       title=title, color_discrete_sequence=color_discrete_sequence)
+
+st.cache_data()
+def create_histogram(df, column, title, color, bins=None):
+    # Create a histogram
+    fig = px.histogram(df, 
+                       x=column,
+                       nbins=bins)  # Here we use the 'bins' variable
+
+    # Set the color of all bars to be the same
+    fig.update_traces(marker=dict(color=color))
+
+    # Rotate the x-axis labels if they are too long
+    fig.update_layout(
+        xaxis=dict(tickangle=45),  # rotate the x-axis labels 45 degrees
+        yaxis_title="Count",
+        autosize=False,  # disable autosizing
+        width=500,  # set figure width
+        height=450,  # set figure height
+        margin=dict(t=10,l=20, r=20, b=120),  # adjust margins to make room for x-axis labels
+    )
+    return fig
+
+st.cache_data()
+def heat_mapper(df, title):
+    """
+    This function takes a DataFrame as input and returns a plotly heatmap figure.
+    The DataFrame should contain 'GRADE', 'OWNER' and 'RISK' columns.
+    """
+    
+    # Create a function to truncate the risk text
+    def truncate_text(text):
+        return ' '.join(text.split()[:5])
+
+    # Truncate the risk text and then group the DataFrame by 'GRADE' and 'OWNER',
+    # concatenating the risks with line breaks
+    df['RISK'] = df['TITLE'].apply(truncate_text)
+    text = df.groupby(['GRADE', 'OWN. FUNC'])['TITLE'].apply(lambda x: '<br>'.join(x)).reset_index()
+
+    # Create a pivot table from the DataFrame for the number of risks
+    pivot = df.pivot_table(index='GRADE', columns='OWN. FUNC', aggfunc='size', fill_value=0)
+
+    # Create a pivot table for the hover text
+    hover_text = text.pivot(index='GRADE', columns='OWN. FUNC', values='TITLE').fillna('')
+
+    # Create the heatmap figure
+    fig = go.Figure(data=go.Heatmap(
+        z=pivot.values,
+        x=pivot.columns,
+        y=pivot.index,
+        text=hover_text.values,  # add the hover text
+        hoverongaps=False,
+        hoverinfo='text',
+        colorscale='RdYlGn_r',
+        ),
+        layout = go.Layout(height=400, width=600)) #Here we set the height to 600
+
+    # Add annotations (the number of risks in each square)
+    annotations = []
+    for i, row in enumerate(pivot.values):
+        for j, value in enumerate(row):
+            annotations.append(go.layout.Annotation(
+                x=pivot.columns[j], y=pivot.index[i], text=str(value),
+                showarrow=False, font=dict(size=14, color='#333333')))
+
+    fig.update_layout(annotations=annotations,
+                      title_text="",
+                      margin=dict(t=10))
+
+    # Return the figure
     return fig
